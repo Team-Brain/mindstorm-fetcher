9041ebff4befd150b4dd760648ef7eeb79b70feb
Marek Abaffy <abaffy.m@gmail.com>
2017-09-13 13:34:05 +0200

    Project structure

---
+++ src/main/java/dk/aau/teambrain/mindstormfetcher/HelloWorld.java
@@ -0,0 +1,22 @@
+package dk.aau.teambrain.mindstormfetcher;
+
+import lejos.hardware.BrickFinder;
+import lejos.hardware.Keys;
+import lejos.hardware.ev3.EV3;
+import lejos.hardware.lcd.TextLCD;
+
+/**
+ * Example leJOS EV3 Project.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        EV3 ev3 = (EV3) BrickFinder.getLocal();
+        TextLCD lcd = ev3.getTextLCD();
+        Keys keys = ev3.getKeys();
+
+        lcd.drawString("Hello World", 4, 4);
+        keys.waitForAnyPress();
+    }
+
+}
